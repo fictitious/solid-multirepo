a5f71db1812fe0d9eed1f17e160c28530fe65f4c
Artem Khodyush <greenkaa@gmail.com>
2022-01-16 12:13:22 -0800

    move app3 example into separate folder

--- examples/devtools-demo1/src/app3/app3.tsx
+++ examples/devtools-demo1/src/app3/app3.tsx
@@ -1,6 +1,10 @@
 
 import type {Component} from 'solid-js';
-import {createSignal, onCleanup, Show} from 'solid-js';
+import {lazy, createSignal, onCleanup, Show} from 'solid-js';
+import {Dynamic} from 'solid-js/web';
+
+import {Red, Green} from './app3-comps';
+
 
 const Comp3: Component<{text: string}> = (props) => {
 
@@ -17,7 +21,7 @@ const Comp3: Component<{text: string}> = (props) => {
             console.log(`Comp3 cleanup`, e.stack);
         }
     });
-    return <span>Comp3: {props.text}</span>;
+    return <span>Comp3 mm: {props.text}</span>;
 }
 
 function createShowSignal() {
@@ -25,8 +29,15 @@ function createShowSignal() {
     return {show, setShow};
 }
 
+
+const RedD = lazy(() => import ('./app3-comps-red'));
+const GreenD = lazy(() => import ('./app3-comps-green'));
+
 const App3: Component = () => {
+
+
     const showSignals = [
+        createShowSignal(),
         createShowSignal(),
         createShowSignal(),
         createShowSignal()
@@ -37,6 +48,9 @@ const App3: Component = () => {
         <div><button onclick={[toggleShow, 0]}>{showSignals[0].show() ? 'Hide' : 'Show'}</button> <Show when={showSignals[0].show()}><Comp3 text="0" /></Show></div>
         <div><button onclick={[toggleShow, 1]}>{showSignals[1].show() ? 'Hide' : 'Show'}</button> <Show when={showSignals[1].show()}><Comp3 text="1" /></Show></div>
         <div><button onclick={[toggleShow, 2]}>{showSignals[2].show() ? 'Hide' : 'Show'}</button> <Show when={showSignals[2].show()}><Comp3 text="2" /></Show></div>
+        <div><button onclick={[toggleShow, 3]}>{showSignals[3].show() ? 'Red' : 'Green'}</button><Dynamic component={showSignals[3].show() ? Red : Green} /></div>
+        <div>x <RedD /></div>
+        <div>y <GreenD /></div>
     </div>
 };
 
--- examples/devtools-demo1/src/index.tsx
+++ examples/devtools-demo1/src/index.tsx
@@ -1,7 +1,7 @@
 import {render} from 'solid-js/web';
 
 import {App1, App2} from './app';
-import {App3} from './app3';
+import {App3} from './app3/app3';
 
 render(() => <App1 />, document.getElementById('root1'));
 render(() => <App2 />, document.getElementById('root2'));
